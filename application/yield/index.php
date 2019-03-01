2808923eb55f7b12a8d8f3958973ea8cc3d45f04
andy <andy@7shangzuo.com>
2019-03-01 18:11:11 +0800

    |++

--- application/yield/index.php
+++ application/yield/index.php
@@ -11,7 +11,7 @@
 <a href="simple.php">普通实现方式</a><br><br>
 <a href="useYield.php">使用yield并理解</a><br><br>
 <a href="readLargeFiles.php">使用yield读取大文件</a><br><br>
-<a href="million.php">百万级别的访问量</a><br><br>
+<a href="million.php">百万级别的访问量，使用yield内存优化</a><br><br>
 <a href="exportUseYield.php">使用yield数据导出</a><br><br>
 </body>
 </html>

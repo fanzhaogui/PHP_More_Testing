b6e4363f3bd4e8546f96eda190958af816008876
fanzhaogui <l>
2019-09-17 19:20:14 +0800

    '这几个js没太看懂'

--- simple_page/spa/js/spa.shell.js
+++ simple_page/spa/js/spa.shell.js
@@ -1,5 +1,9 @@
 spa.shell = (function () {
     var configMap = {
+        anchor_schema_map : {
+            // 定义给uriAnchor使用的映射，用于验证
+            chat : {open : true, close : true}
+        },
         main_html : String()
         + '<div class="spa-shell-head">'
             + '<div class="spa-shell-head-logo"></div>'
@@ -24,12 +28,59 @@ spa.shell = (function () {
         // 状态等
         stateMap = {
             $container : null,
+            anchor_map : {},
             is_chat_retracted : true // 聊天滑块是否关闭
         },
         jqueryMap = {},
         // 事件
         setJquery, toggleChat, onClickChat,
+        copyAnchorMap, changeAnchorPart, onHashchange,
         initModule;
+
+    copyAnchorMap = function () {
+        return $.extend( true, {}, stateMap.anchor_map );
+    };
+
+    changeAnchorPart = function (arg_map) {
+        var
+        anchor_map_revise = copyAnchorMap(),
+        bool_return = true,
+        key_name, key_name_dep,
+
+        KEYVAL;
+        for (key_name in arg_map) {
+            if (arg_map.hasOwnProperty(key_name)) {
+
+                if (key_name.indexOf('_') === 0) {
+                    // continue KEYVAL;
+                    continue ;
+                }
+                anchor_map_revise[key_name] = arg_map[key_name];
+
+                key_name_dep = "_" + key_name;
+
+                if (arg_map[key_name_dep]) {
+                    anchor_map_revise[key_name_dep] = arg_map[key_name_dep]
+                }
+                else {
+                    delete anchor_map_revise[key_name_dep];
+                    delete anchor_map_revise['_s' + key_name_dep];
+                }
+            }
+
+        }
+        try {
+            $.uriAnchor.setAnchor(anchor_map_revise);
+        } catch (err) {
+            $.uriAnchor.setAnchor(stateMap.anchor_map, null, true);
+            bool_return = false;
+        }
+        return bool_return;
+    };
+
+    onHashchange = function (event) {
+
+    };
 
     setJquery = function () {
         var $container = stateMap.$container;
@@ -81,7 +132,13 @@ spa.shell = (function () {
 
     // 聊天滑块点击
     onClickChat = function (event) {
-        toggleChat(stateMap.is_chat_retracted);
+        if ( toggleChat(stateMap.is_chat_retracted) ) {
+            // URL增加锚点
+            console.log(stateMap.is_chat_retracted)
+            $.uriAnchor.setAnchor({
+                chat: (stateMap.is_chat_retracted ? 'open' : 'closed')
+            })
+        }
         return false;
     };
 
